f18371c57c7c864081c40aaf848d2d4dfeeff238
NIHAMATHULLAHdev <nihamathullah007@gmail.com>
2024-12-26 23:46:01 +0530

    secure file as console not open

---
+++ js/secure.js
@@ -0,0 +1,11 @@
+// Disable right-click
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Disable specific key combinations
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+    }
+});
